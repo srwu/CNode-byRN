d731264324a700771e107f68b9a033166f3a7416
吴双熔 <wushuangrong@jd.com>
2015-09-18 17:03:55 +0800

    public

--- publicTopic.js
+++ publicTopic.js
@@ -9,6 +9,7 @@ var {
     StyleSheet,
     Text,
     View,
+    TextInput,
 } = React;
 
 var publicTopic = React.createClass({
@@ -16,9 +17,41 @@ var publicTopic = React.createClass({
 render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          {'发布主题(开发中)'}
-        </Text>
+        <TextInput
+          autoCapitalize="none"
+          placeholder="主题名"
+          autoCorrect={false}
+          // onFocus={() => this.updateText('onFocus')}
+          // onBlur={() => this.updateText('onBlur')}
+          // onChange={(event) => this.updateText(
+          //   'onChange text: ' + event.nativeEvent.text
+          // )}
+          // onEndEditing={(event) => this.updateText(
+          //   'onEndEditing text: ' + event.nativeEvent.text
+          // )}
+          // onSubmitEditing={(event) => this.updateText(
+          //   'onSubmitEditing text: ' + event.nativeEvent.text
+          // )}
+          style={styles.textInput}/>
+
+          <TextInput
+          autoCapitalize="none"
+          placeholder="主题名"
+          autoCorrect={false}
+          // onFocus={() => this.updateText('onFocus')}
+          // onBlur={() => this.updateText('onBlur')}
+          // onChange={(event) => this.updateText(
+          //   'onChange text: ' + event.nativeEvent.text
+          // )}
+          // onEndEditing={(event) => this.updateText(
+          //   'onEndEditing text: ' + event.nativeEvent.text
+          // )}
+          // onSubmitEditing={(event) => this.updateText(
+          //   'onSubmitEditing text: ' + event.nativeEvent.text
+          // )}
+          style={styles.textInput2}/>
+
+        
       </View>
     );
 },
@@ -28,13 +61,35 @@ render: function() {
 var styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
         backgroundColor: '#F5FCFF',
-        height:80,
+        //justifyContent: 'flex-start',
     },
 
+    textInput: {
+        
+        borderWidth: 0.5,
+        borderColor: '#0000ff',
+        padding: 20,
+        fontSize: 14,
+        top:84,
+        left:10,
+        height: 40,
+        width:300,
+      },
+
+      textInput2: {
+        marginTop: 100,
+        borderWidth: 0.5,
+        borderColor: '#ff0000',
+        padding: 20,
+        //flex: 1,
+        fontSize: 20,
+        left:10,
+        height: 200,
+        //marginRight:10,
+        width:300,
+      },
     title:{
       fontFamily: 'Cochin',
     },
